9b494f69c7d83543d342c4c632fa5e2aa6c03066
gaddamanand <gaddamanand13@gmail.com>
2022-12-29 10:01:45 +0700

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "Anand", "Anand!@34", "avmsdb");
+$con=mysqli_connect("localhost", "Aryan", "zAxrcyvabn123", "avmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
